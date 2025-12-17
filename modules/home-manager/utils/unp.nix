8ed59fd6ba3c09eed77ccc3a1910ab0e9cebb1df
m3l6h <8094643+M3L6H@users.noreply.github.com>
2025-12-17 10:22:01 -0600

    chore: add unrar alias for unrar-free

--- modules/home-manager/utils/unp.nix
+++ modules/home-manager/utils/unp.nix
@@ -1,9 +1,16 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   options = {
     utils.unp.enable = lib.mkEnableOption "enables unp module";
   };
 
   config = lib.mkIf config.utils.unp.enable {
+    home.shellAliases.unrar = "unrar-free";
     home.packages = with pkgs; [
       p7zip # For unpacking 7z archives
       unrar-free # For unpacking rar archives
@@ -12,4 +19,3 @@
     ];
   };
 }
-
