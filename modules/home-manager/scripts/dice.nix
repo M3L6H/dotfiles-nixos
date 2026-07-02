4204543985d1e3fb0932813d0031146b0461fa8f
m3l6h <8094643+M3L6H@users.noreply.github.com>
2026-07-02 18:28:24 -0500

    feat: add <A-k>/<A-j> keybinds to rofi
    Also:
    - Make dice program actually center number

--- modules/home-manager/scripts/dice.nix
+++ modules/home-manager/scripts/dice.nix
@@ -11,33 +11,53 @@
             # Declare required Python libraries here
             libraries = with pkgs.python3Packages; [
               pyfiglet
+              readchar
             ];
           }
           ''
+            import argparse
             import os
             import random
             import shutil
-            import sys
 
             import pyfiglet
+            import readchar
 
 
             def dice(sides=6):
-                os.system("cls" if os.name == "nt" else "clear")
+                os.system('cls' if os.name == 'nt' else 'clear')
 
                 num = random.randint(1, int(sides))
                 ascii_art = pyfiglet.figlet_format(str(num))
 
-                terminal_columns = shutil.get_terminal_size().columns
-                for line in ascii_art.splitlines():
-                    print(line.center(terminal_columns))
+                terminal_size = shutil.get_terminal_size()
+                lines = ascii_art.splitlines()
+                before = (terminal_size.lines - len(lines)) // 2
+                for _ in range(before):
+                    print()
+                for line in lines:
+                    print(line.center(terminal_size.columns))
 
 
             def main():
-                dice(*sys.argv[1:])
+                parser = argparse.ArgumentParser(description='Terminal-based die')
+                parser.add_argument(
+                    'sides',
+                    type=int,
+                    default=6,
+                    nargs='?',
+                    help="Sides of the dice")
+                args = vars(parser.parse_args())
+
+                while True:
+                    dice(**args)
+                    char = readchar.readchar()
+
+                    if char == 'q':
+                        return
 
 
-            if __name__ == "__main__":
+            if __name__ == '__main__':
                 main()
           '';
     in
